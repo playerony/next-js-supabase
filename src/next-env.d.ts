e8797b37773a1b108b519ccc7a6e5c336d2eaf2e
playerony <pawel.wojtasinski.1995@gmail.com>
2022-01-24 18:54:28 +0100

    feat: create stripe account route

--- src/next-env.d.ts
+++ src/next-env.d.ts
@@ -4,6 +4,9 @@
 declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface ProcessEnv {
+    API_ROUTE_SECRET: string;
+    STRIPE_SECRET_KEY: string;
+    NEXT_PUBLIC_STRIPE_KEY: string;
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_KEY: string;
   }
